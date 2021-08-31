77daed4b5f5d5993303d4b64e6613fe928f4be54
LeeJuHyun <50076031+JuHyun419@users.noreply.github.com>
2021-09-01 00:35:00 +0900

    Create RssFeeds.java

---
+++ RssFeeds.java
@@ -0,0 +1,72 @@
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+
+public class RssFeeds {
+  
+    public static final String URL = "https://zzang9ha.tistory.com/rss";
+    public static final int MAX_POST = 5;
+
+    public static final String README = "# \uD83D\uDC14 \uD83D\uDC1D \uD83D\uDC1C\n" +
+            "\n" +
+            "<p align=\"center\">\n" +
+            " <img width=\"900\" height=\"300\" alt=\"mungmung\" src=\"https://user-images.githubusercontent.com/50076031/118226149-615b6700-b4c1-11eb-8252-6c7aa5f0a5bb.gif\">\n" +
+            "</p>\n" +
+            "<p align=\"center\"> \n" +
+            "  <img width=\"500\" height=\"200\" alt=\"Github status\" src=\"https://github-readme-stats.vercel.app/api?username=JuHyun419&count_private=true&theme=radical\">\n" +
+            "</p>\n" +
+            "\n" +
+            "\n" +
+            "<div align=center>\n" +
+            "  \n" +
+            "  [![Tistory Blog](http://img.shields.io/badge/-Tistory%20Blog-blue?style=flat&logo=Blogger&link=https://zzang9ha.tistory.com/)](https://zzang9ha.tistory.com/) \n" +
+            "  [![Naver Blog](http://img.shields.io/badge/-Naver%20Blog-green?style=flat&logo=Blogger&link=https://blog.naver.com/zzang9ha)](https://blog.naver.com/zzang9ha) \n" +
+            "  [![Study](http://img.shields.io/badge/-Study%20-655ced?style=flat&logo=github&link=https://github.com/JuHyun419/study)](https://github.com/JuHyun419/study) \n" +
+            "  [![Project](http://img.shields.io/badge/-Project-ff69b4?style=flat&logo=github&link=https://github.com/jh-project-repo)](https://github.com/jh-project-repo) \n" +
+            "  [![Gmail](http://img.shields.io/badge/Gmail-important?style=flat&logo=Gmail&link=mailto:zzang9haha@gmail.com)](mailto:zzang9haha@gmail.com) \n" +
+            "\n" +
+            "</div>\n" +
+            "\n" +
+            "<div align=center>\n" +
+            " \n" +
+            "[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FJuHyun419&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)\n" +
+            " \n" +
+            "</div>\n" +
+            " \n" +
+            "<br>\n" +
+            " \n" +
+            "\uD83D\uDCDA Latest Blog Post\n";
+
+    public static void main(String[] args) throws IOException, FeedException {
+        SyndFeedInput syndFeedInput = new SyndFeedInput();
+        SyndFeed feed = syndFeedInput.build(new XmlReader(new URL(URL)));
+        StringBuilder blogPosts = new StringBuilder();
+
+        for (int i = 0; i < feed.getEntries().size(); i++) {
+            if (i >= MAX_POST) {
+                break;
+            }
+            SyndEntry entry = feed.getEntries().get(i);
+            blogPosts.append(getBlogPost(entry));
+        }
+
+        String readmeText = README + blogPosts.toString();
+
+        File file = new File("README.md");
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(readmeText);
+        fileWriter.close();
+    }
+
+    /* ex) [`여름이 떠나가는 8월의 회고`](https://zzang9ha.tistory.com/379) <br/>  */
+    private static String getBlogPost(final SyndEntry entry) {
+        return "[`" + entry.getTitle() + "`](" + entry.getLink() + ") <br/> \n";
+    }
+}
